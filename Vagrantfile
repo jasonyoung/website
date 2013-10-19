239185b75ccb46dfa7b786b2fb1e731b103e2d7c
Jason Young <youngjase82@gmail.com>
2013-10-19 20:52:38 +0100

    Hello world!

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,7 @@ Vagrant.configure("2") do |config|
   config.vm.box = "precise64"
   config.vm.box_url = "http://files.vagrantup.com/precise64.box"
   
-  config.vm.synced_folder "./", "/home/vagrant/workspace"
+  config.vm.synced_folder "./src", "/home/vagrant/workspace"
 
   config.vm.network "forwarded_port", guest: 3000, host: 3000
 
---
+++ src/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+//Tell Express to use the Jade templating engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+//Log request information to the console
+app.use(express.logger('dev'))
+
+app.get('/', function(req, res) {
+	res.render('hello');	
+});
+
+app.listen(3000);
+console.log('Listening on port 3000...');
