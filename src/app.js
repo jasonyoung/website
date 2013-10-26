060d46f294319882b754d8109a12055a2405238e
Paul Daigneault <paul.daigneault@gmail.com>
2013-10-26 17:28:55 +0100

    Connect to MongoDB on app startup

--- src/app.js
+++ src/app.js
@@ -1,40 +1,47 @@
 var express = require('express');
 var swig = require('swig');
 var app = express();
+var MongoClient = require('mongodb');
 
-app.engine('html', swig.renderFile);
+MongoClient.connect("mongodb://localhost:27017/babyPoolDB", function (err, db) {
+    if (err) throw err;
 
-//Tell Express to use the Swig templating engine
-app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
+    console.log('Connected!!');
 
-app.set('view cache', false);
-swig.setDefaults({ cache: false });
+    app.engine('html', swig.renderFile);
 
-//Log request information to the console
-app.use(express.logger('dev'));
+    //Tell Express to use the Swig templating engine
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'html');
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.bodyParser());
+    app.set('view cache', false);
+    swig.setDefaults({ cache: false });
 
-app.get('/', function(req, res) {
-	res.render('home');
+    //Log request information to the console
+    app.use(express.logger('dev'));
+
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.bodyParser());
+
+    app.get('/', function (req, res) {
+        res.render('home');
+    });
+
+    app.get('/newpool', function (req, res) {
+        res.render('newPool');
+    });
+
+    app.post('/newpool', function (req, res) {
+        console.log('Your name:');
+        console.log(req.body.userName);
+
+        res.send('Success!!');
+    });
+
+    app.get('/joinpool', function (req, res) {
+        res.render('joinPool');
+    });
+
+    app.listen(3000);
+    console.log('Listening on port 3000...');
 });
-
-app.get('/newpool', function(req, res) {
-	res.render('newPool');
-});
-
-app.post('/newpool', function(req, res) {
-    console.log('Your name:');
-    console.log(req.body.userName);
-
-	res.send('Success!!');
-});
-
-app.get('/joinpool', function(req, res) {
-    res.render('joinPool');
-});
-
-app.listen(3000);
-console.log('Listening on port 3000...');
