2a09967f5e4252a32508fa4c48d92078b47b35ac
Jason Young <youngjase82@gmail.com>
2013-10-27 16:34:34 +0000

    Added explaining comments.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var routes = require('./routes');
 
+// Function to connect to the database.
 var connectToDB = function(callback) {
 	MongoClient.connect("mongodb://localhost:27017/babyPoolDB", function(err, db) {
 		if (err) {
@@ -12,40 +13,48 @@ var connectToDB = function(callback) {
 
 		console.log('Successfully connected to DB');
 
+		// Close the connection to the DB when the application terminates.
 		process.on('SIGTERM', function() {
 			db.close();
 		});
 
+		// Invoke the callback, passing the connected db instance.
 		process.nextTick(function() {
 			callback(null, db)
 		});
 	});
 };
 
+// Function to initialise application.
 var init = function(err, db) {
 	if (err) throw err;
 
-	app.engine('html', swig.renderFile);
-
-	//Tell Express to use the Swig templating engine
+	// Configure templating engine.
+	app.engine('html', swig.renderFile);	
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'html');
 
+	// Disable view cache. Useful when developing.
 	app.set('view cache', false);
 	swig.setDefaults({
 		cache: false
 	});
 
-	//Log request information to the console
+	// Log request information to the console.
 	app.use(express.logger('dev'));
 
+	// Set location of static resources.
 	app.use(express.static(__dirname + '/public'));
+
+	// Express middleware to populate 'req.body' so we can access POST variables.
 	app.use(express.bodyParser());
 
+	// Setup application routes.
 	routes(app, db);	
 
 	app.listen(3000);
 	console.log('Listening on port 3000...');
 };
 
+// Start doing stuff! Pass init function as callback to connectToDB function.
 connectToDB(init);
