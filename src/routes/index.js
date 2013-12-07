52d19c78fcbede29eccd44b1fff3e720a71ae29f
Jason Young <youngjase82@gmail.com>
2013-12-07 16:50:57 +0000

    Added basic auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 var SiteHandler = require('./site');
 var PoolHandler = require('./pool');
 
-module.exports = function(app, db) {
+module.exports = function(app, db, passport) {
 	var siteHandler = new SiteHandler();
 	var poolHandler = new PoolHandler(db);
 
 	app.get('/', siteHandler.displayMainPage);
 	app.get('/newpool', poolHandler.displayNewPoolPage);
 	app.post('/newpool', poolHandler.handleNewPool);
-	app.get('/joinpool', poolHandler.displayJoinPoolPage);
+	app.get('/joinpool', poolHandler.displayJoinPoolPage);	
+
+	app.post("/login", passport.authenticate('local'));
 };
